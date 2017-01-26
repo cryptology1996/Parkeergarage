5bf7e050478a9eb74cebecf4e5488070e2203ddd
Cryptology1996 <a.van.der.berg@st.hanze.nl>
2017-01-26 14:01:41 +0100

    First Abstract View.java

---
+++ src/AbstractView.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+/**
+ * Abstract class for the View subclasses
+ * @author Martijn Bakker, Daniel Bouius, Arneld van der Veen, Antonie Groenveld, Albert van der Berg
+ *
+ */
+public abstract class AbstractView extends JPanel{
+	
+	/**
+	 * constructor for class Abstractview
+	 */
+	public AbstractView() {
+		
+	}
+
+}
