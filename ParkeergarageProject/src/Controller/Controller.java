715142fd4d78fb7e40bf86d6d2e1d2993ba1e708
DanielBouius <d.bouius@st.hanze.nl>
2017-01-30 15:52:15 +0100

    Update Controller.java

--- ParkeergarageProject/src/Controller/Controller.java
+++ ParkeergarageProject/src/Controller/Controller.java
@@ -10,41 +10,89 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.*;
 
-public class Controller extends AbstractController {
+public class Controller extends AbstractController implements ActionListener{
 	
 	private JButton een;
 	private JButton honderd;
 	private JButton start;
 	private JButton stop;
+	private ActionEvent event;
+	private Simulator model;
 
 public Controller(Simulator model) {
 		super(model);
+		this.model=model;
     	setBackground(Color.LIGHT_GRAY);
     	setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY,8));
     	
     	een = new JButton("+1");
+    	een.addActionListener((ActionListener) this);
         een.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
         add(een);
          
         honderd= new JButton("+100");
+        honderd.addActionListener((ActionListener) this);
         honderd.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
         add(honderd);
        
         start= new JButton("Start");
+        start.addActionListener((ActionListener) this);
         honderd.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
         add(start);
        
         stop= new JButton("Stop");
+        stop.addActionListener((ActionListener) this);
         honderd.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
         add(stop);
         
            setVisible(true);
 		
 	}
-public void run() {
-    for (int i = 0; i < 10000; i++) {
-        model.tick();
-    }
+public void setActionEvent(ActionEvent e) {
+	event = e;
 }
+
+/**
+ * returns the set event
+ * @return event
+ */
+public ActionEvent getActionEvent() {
+	return event;
 }
+
+/**
+ * Executes the the input action
+ */
+public void actionPerformed(ActionEvent e)
+{
+	// sets the received actionEvent, and creates a new thread.
+	setActionEvent(e);
+	Thread performerThread = new Thread(){
+	
+
+	
+	public void run(){
+		ActionEvent e = getActionEvent();
+		String command = e.getActionCommand();
+	
+		if (command == "+100"){
+			model.runCommand(100);
+		}
+		if (command == "+1"){
+			model.runCommand(1);
+		}
+		if (command == "Start"){
+			model.runCommand(1000);
+		}
+		if (command == "Stop"){
+			model.runCommand(0);
+		}
+		
+	}
+	};
+	performerThread.start();
+}
+
+}
+
 
