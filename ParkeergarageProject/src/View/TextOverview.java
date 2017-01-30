2732aff42dd72cea65cbf04636d7157427400cca
DanielBouius <d.bouius@st.hanze.nl>
2017-01-30 12:12:42 +0100

    Update TextOverview.java

--- ParkeergarageProject/src/View/TextOverview.java
+++ ParkeergarageProject/src/View/TextOverview.java
@@ -1,72 +1,204 @@
-package View;
-import Model.*;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+package Model;
 
+import java.util.Random;
+import View.*;
 
-import javax.swing.BorderFactory;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+public class Simulator extends AbstractModel {
+	private static final String AD_HOC = "1";
+	private static final String PASS = "2";
+	private static final String SUB = "3";
+	
+	
+	private CarQueue entranceCarQueue;
+    private CarQueue entrancePassQueue;
+    private CarQueue paymentCarQueue;
+    private CarQueue exitCarQueue;
+    private SimulatorView simulatorView;
+    private int PayingCars;
+    private TextOverview textOverview;
 
-public class TextOverview extends JPanel {
-	private JTextField txtRevenue;
-	private Simulator model;
-	double ticketPrice = 7.50;
-	
-	
-	
-	/**
-	 * Create the frame.
-	 */
-	public TextOverview(Simulator model) {
-		this.model = model;
-		this.setLayout(new GridLayout(0,1)); 
-		//setBounds(100, 100, 450, 339);
-		
-		
-		
-		
-		
-		JPanel revenue = new JPanel();
-		revenue.setLayout(new FlowLayout());
-		revenue.setBackground(Color.LIGHT_GRAY);
-		JLabel lblRevenue = new JLabel("Total revenue");
-		txtRevenue = new JTextField();
-		txtRevenue.setEditable(false);
-		txtRevenue.setColumns(10);
-		txtRevenue.setBackground(Color.LIGHT_GRAY);
-		txtRevenue.setBorder(BorderFactory.createLineBorder(Color.GRAY,1));
-		revenue.add(lblRevenue);
-		revenue.add(txtRevenue);
-		this.add(revenue);
-		
+    private int day = 0;
+    private int hour = 0;
+    private int minute = 0;
 
-		// using the calc function to calc the revenue
-		calcRevenue();
+    private int tickPause = 100;
 
-	}
-	
-	/**
-	 * updates the values inside the textFields
-	 */
-	public void updateView(){
-		calcRevenue();
+    int weekDayArrivals= 100; // average number of arriving cars per hour
+    int weekendArrivals = 200; // average number of arriving cars per hour
+    int weekDayPassArrivals= 50; // average number of arriving cars per hour
+    int weekendPassArrivals = 5; // average number of arriving cars per hour
 
-	}	
-	
-	/**
-	 * calculates the revenue
-	 */
-	public void calcRevenue()
-	{
-	
-	int totalCars = model.getPayingCars();	
-	double totalRevenue = totalCars * ticketPrice;
-			
-		txtRevenue.setText(" "+totalRevenue+"$");	
-		
-	}
-	
+    int enterSpeed = 3; // number of cars that can enter per minute
+    int paymentSpeed = 7; // number of cars that can pay per minute
+    int exitSpeed = 5; // number of cars that can leave per minute
+    
+
+    public Simulator() {
+        entranceCarQueue = new CarQueue();
+        entrancePassQueue = new CarQueue();
+        paymentCarQueue = new CarQueue();
+        exitCarQueue = new CarQueue();
+        textOverview = new TextOverview(this);
+        //test
+        simulatorView = new SimulatorView(3, 6, 30, textOverview);
+    }
+    public void tick() {
+    	advanceTime();
+    	handleExit();
+    	updateViews();
+    	// Pause.
+        try {
+            Thread.sleep(tickPause);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    	handleEntrance();
+    }
+
+    private void advanceTime(){
+        // Advance the time by one minute.
+        minute++;
+        while (minute > 59) {
+            minute -= 60;
+            hour++;
+        }
+        while (hour > 23) {
+            hour -= 24;
+            day++;
+        }
+        while (day > 6) {
+            day -= 7;
+        }
+
+    }
+
+    private void handleEntrance(){
+    	carsArriving();
+    	carsEntering(entrancePassQueue);
+    	carsEntering(entranceCarQueue);  	
+    }
+    
+    private void handleExit(){
+        carsReadyToLeave();
+        carsPaying();
+        carsLeaving();
+    }
+    
+    private void updateViews(){
+    	simulatorView.tick();
+        // Update the car park view.
+        simulatorView.updateView();	
+    }
+    
+    private void carsArriving(){
+    	int numberOfCars=getNumberOfCars(weekDayArrivals, weekendArrivals);
+        addArrivingCars(numberOfCars, AD_HOC);    	
+    	numberOfCars=getNumberOfCars(weekDayPassArrivals, weekendPassArrivals);
+        addArrivingCars(numberOfCars, PASS);    	
+        numberOfCars=getNumberOfCars(weekDayPassArrivals, weekendPassArrivals);
+        addArrivingCars(numberOfCars, SUB);  
+    }
+
+    private void carsEntering(CarQueue queue){
+        int i=0;
+        // Remove car from the front of the queue and assign to a parking space.
+    	while (queue.carsInQueue()>0 && 
+    			simulatorView.getNumberOfOpenSpots()>0 && 
+    			i<enterSpeed) {
+            Car car = queue.removeCar();
+            Location freeLocation = simulatorView.getFirstFreeLocation();
+            simulatorView.setCarAt(freeLocation, car);
+            i++;
+        }
+    }
+    
+    private void carsReadyToLeave(){
+        // Add leaving cars to the payment queue.
+        Car car = simulatorView.getFirstLeavingCar();
+        while (car!=null) {
+        	if (car.getHasToPay()){
+	            car.setIsPaying(true);
+	            paymentCarQueue.addCar(car);
+        	}
+        	else {
+        		carLeavesSpot(car);
+        	}
+            car = simulatorView.getFirstLeavingCar();
+        }
+    }
+
+    private void carsPaying(){
+        // Let cars pay.
+    	for (int i = 0; i < paymentSpeed; i++) {
+            Car car = paymentCarQueue.removeCar();
+            if (car != null){
+            	PayingCars++;
+            	
+            }
+            if (car == null) {
+                break;
+            }
+            simulatorView.removeCarAt(car.getLocation());
+            exitCarQueue.addCar(car);
+            textOverview.updateView();
+    	
+    	}
+    }
+    
+    private void carsLeaving(){
+        // Let cars leave.
+    	int i=0;
+    	while (exitCarQueue.carsInQueue()>0 && i < exitSpeed){
+            exitCarQueue.removeCar();
+            i++;
+    	}	
+    }
+    
+    private int getNumberOfCars(int weekDay, int weekend){
+        Random random = new Random();
+
+        // Get the average number of cars that arrive per hour.
+        int averageNumberOfCarsPerHour = day < 5
+                ? weekDay
+                : weekend;
+
+        // Calculate the number of cars that arrive this minute.
+        double standardDeviation = averageNumberOfCarsPerHour * 0.3;
+        double numberOfCarsPerHour = averageNumberOfCarsPerHour + random.nextGaussian() * standardDeviation;
+        return (int)Math.round(numberOfCarsPerHour / 60);	
+    }
+    /*
+     * returns the amount of cars that will pay 
+     * @return PayingCars
+     */
+    public int getPayingCars() {
+    	return PayingCars;
+    }
+    
+    private void addArrivingCars(int numberOfCars, String type){
+        // Add the cars to the back of the queue.
+    	switch(type) {
+    	case AD_HOC: 
+            for (int i = 0; i < numberOfCars; i++) {
+            	entranceCarQueue.addCar(new AdHocCar());
+            }
+            break;
+    	case PASS:
+            for (int i = 0; i < numberOfCars; i++) {
+            	entrancePassQueue.addCar(new ParkingPassCar());
+            }
+    	    break;
+    	case SUB:
+    		for (int i = 0; i < numberOfCars; i++) {
+    			entrancePassQueue.addCar(new SubscriberPassCar());
+    		}
+    		break;
+    	}
+    }
+    
+    private void carLeavesSpot(Car car){
+    	simulatorView.removeCarAt(car.getLocation());
+        exitCarQueue.addCar(car);
+    }
+    
 }
